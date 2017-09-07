750d2fb1003809507ddde2dff6a2edaaa750be8e
Raymond Chua <ray.r.chua@gmail.com>
2017-09-07 18:48:07 +0200

    comment out testing

--- cifar10_classifier/cifar10_main.py
+++ cifar10_classifier/cifar10_main.py
@@ -96,22 +96,22 @@ _, predicted = torch.max(outputs.data, 1)
 
 print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
 
-class_correct = list(0. for i in range(10))
-class_total = list(0. for i in range(10))
-for data in testloader:
-	images, labels = data
-	outputs = net(Variable(images.cuda()))
-	_, predicted = torch.max(outputs.data, 1)
-	c = (predicted == labels).squeeze()
-	for i in range(4):
-		label = labels[i]
-		class_correct[label] += c[i]
-		class_total[label] += 1
+# class_correct = list(0. for i in range(10))
+# class_total = list(0. for i in range(10))
+# for data in testloader:
+# 	images, labels = data
+# 	outputs = net(Variable(images.cuda()))
+# 	_, predicted = torch.max(outputs.data, 1)
+# 	c = (predicted == labels).squeeze()
+# 	for i in range(4):
+# 		label = labels[i]
+# 		class_correct[label] += c[i]
+# 		class_total[label] += 1
 
 
-for i in range(10):
-	print('Accuracy of %5s : %2d %%' % (
-		classes[i], 100 * class_correct[i] / class_total[i]))
+# for i in range(10):
+# 	print('Accuracy of %5s : %2d %%' % (
+# 		classes[i], 100 * class_correct[i] / class_total[i]))
 
 
 
