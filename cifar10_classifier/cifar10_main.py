4fda0657d8edf618f716a1f5df3715af6b020855
Raymond Chua <ray.r.chua@gmail.com>
2017-09-07 15:14:20 +0200

    added dl datasets

--- cifar10_classifier/cifar10_main.py
+++ cifar10_classifier/cifar10_main.py
@@ -1,3 +1,24 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
+
+
+transform = transforms.Compose(
+	[transforms.ToTensor(), #convert rgb image to float [0.0, 1.0]
+	transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]) #Given mean and std. dev, normalize each channel
+
+#download the cifar10 training dataset and apply the transform
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+
+#load the training set, data reshuffled at every epoch , set batch size to 4 and num_workers determine
+#how many subprocesses to use for data loading
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+
+#download and load the test set. Same transform applies for test set except that no reshuffling is applied
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+
+#define the classes of the cifar10 dataset
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
