65349cf9bbef86adbebc6ea96a62e52294afb2dc
Raymond Chua <ray.r.chua@gmail.com>
2017-09-07 16:01:27 +0200

    added optimizer and cnn

--- cifar10_classifier/cifar10_main.py
+++ cifar10_classifier/cifar10_main.py
@@ -2,6 +2,13 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+import cifar10_net as net
+
+import torch.optim as optim
+
 
 transform = transforms.Compose(
 	[transforms.ToTensor(), #convert rgb image to float [0.0, 1.0]
@@ -20,5 +27,58 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, n
 
 #define the classes of the cifar10 dataset
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# # functions to show an image
+# def imshow(img):
+# 	img = img / 2 + 0.5     # unnormalize
+# 	npimg = img.numpy()
+# 	plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+
+# # get some random training images
+# dataiter = iter(trainloader)
+# images, labels = dataiter.next()
+
+# # show images
+# imshow(torchvision.utils.make_grid(images))
+# # print labels
+# print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+# plt.show()
+
+#use the cross entropy loss as our loss function and SGD as our optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+num_iter = 2
+
+for epoch in range(num_iter):
+	running_loss = 0.0
+	for i, data in enumerate(trainloader, 0):
+		 # get the inputs
+		inputs, labels = data
+
+		# wrap them in Variable
+		inputs, labels = Variable(inputs), Variable(labels)
+
+		# zero the parameter gradients
+		optimizer.zero_grad()
+
+		 # forward + backward + optimize
+		outputs = net(inputs)
+
+		loss = criterion(outputs, labels)
+		loss.backward()
+		optimizer.step()
+
+		# print statistics
+		running_loss += loss.data[0]
+		if i % 2000 == 1999:    # print every 2000 mini-batches
+			print('[%d, %5d] loss: %.3f' %
+				(epoch + 1, i + 1, running_loss / 2000))
+			running_loss = 0.0
+
+print('Finished Training')
+
+
 
 
---
+++ cifar10_classifier/cifar10_net.py
@@ -0,0 +1,23 @@
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+	def __init__(self):
+		super(Net, self).__init__()
+		self.conv1 = nn.Conv2d(3, 6, 5) #5x5 kernel with input channel 3 and output channel 6
+		self.pool = nn.MaxPool2d(2, 2) #2x2 max pooling with stride 2
+		self.conv2 = nn.Conv2d(6, 16, 5) #5x5 kernel with input channel 6 and output channel 16
+		self.fc1 = nn.Linear(16 * 5 * 5, 120) #applies linear transformation such at y=Ax+b, (16*5*5) input channel, 120 output channels
+		self.fc2 = nn.Linear(120, 84) #applies linear transformation such at y=Ax+b, 120 input channels, 84 output channels
+		self.fc3 = nn.Linear(84, 10) #applies linear transformation such at y=Ax+b, 84 input channels, 10 output channels
+
+    def forward(self, x):
+		x = self.pool(F.relu(self.conv1(x))) #2d conv -> relu -> max pooling
+		x = self.pool(F.relu(self.conv2(x))) #2d conv -> relu -> max pooling
+		x = x.view(-1, 16 * 5 * 5) #returns same data but different size, looks like tf.reshape
+		x = F.relu(self.fc1(x)) #fully-connected -> relu
+		x = F.relu(self.fc2(x)) #fully-connected -> relu
+		x = self.fc3(x) #fully-connected
+		return x
+
